d053cfa8c58fce08b90d676a24dfa4780e569cff
AaronHProgramming <aaronnhager@yahoo.com>
2016-03-11 21:39:31 -0600

    Initial layout of classes

---
+++ app/src/main/java/ahager3/calendarapp/CalendarActivity.java
@@ -0,0 +1,11 @@
+package ahager3.calendarapp;
+
+/**
+ * Created by aaron on 3/11/16.
+ */
+public class CalendarActivity {
+
+    // Accept month and day
+    // Go to a fragment to update or read the events for that day
+
+}
---
+++ app/src/main/java/ahager3/calendarapp/MainActivity.java
@@ -0,0 +1,26 @@
+package ahager3.calendarapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            return;
+        }
+    }
+
+    public void playPressed(View view) {
+        //TODO: Put to start game play activity
+        Intent startGame = new Intent(this, CalendarActivity.class);
+        startActivity(startGame);
+    }
+}
